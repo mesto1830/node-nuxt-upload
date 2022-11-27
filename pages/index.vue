3ede2987d17a49b1e5c0ee0b3b5395fd95faf82c
mesto1830 <mesto1830@outlook.com>
2022-11-28 01:23:23 +0300

    start

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="section">
+  <div class="container">
+    <upload />
+  </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
