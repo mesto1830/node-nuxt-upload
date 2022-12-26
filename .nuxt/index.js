a5d876749936d6072ced531e63dcea6c16ca4b40
mesto1830 <97528383+mesto1830@users.noreply.github.com>
2022-12-26 09:25:28 -0500

    mm

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -12,8 +12,8 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 
 /* Plugins */
 
-import nuxt_plugin_plugin_403f1d56 from 'nuxt_plugin_plugin_403f1d56' // Source: ./components/plugin.js (mode: 'all')
-import nuxt_plugin_axios_e06b982a from 'nuxt_plugin_axios_e06b982a' // Source: ./axios.js (mode: 'all')
+import nuxt_plugin_plugin_6abd960a from 'nuxt_plugin_plugin_6abd960a' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_axios_35590020 from 'nuxt_plugin_axios_35590020' // Source: ./axios.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -177,12 +177,12 @@ async function createApp(ssrContext, config = {}) {
   }
   // Plugin execution
 
-  if (typeof nuxt_plugin_plugin_403f1d56 === 'function') {
-    await nuxt_plugin_plugin_403f1d56(app.context, inject)
+  if (typeof nuxt_plugin_plugin_6abd960a === 'function') {
+    await nuxt_plugin_plugin_6abd960a(app.context, inject)
   }
 
-  if (typeof nuxt_plugin_axios_e06b982a === 'function') {
-    await nuxt_plugin_axios_e06b982a(app.context, inject)
+  if (typeof nuxt_plugin_axios_35590020 === 'function') {
+    await nuxt_plugin_axios_35590020(app.context, inject)
   }
 
   // Lock enablePreview in context
--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,7 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _639ad953 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _6fe593be = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
 
@@ -19,7 +19,7 @@ export const routerOptions = {
 
   routes: [{
     path: "/",
-    component: _639ad953,
+    component: _6fe593be,
     name: "index"
   }],
 
